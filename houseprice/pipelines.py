947a8f107661354fe121da45bb77391ac07ca209
matrix-yang <739260804@qq.com>
2017-11-14 11:28:32 +0800

    redis连接

--- houseprice/pipelines.py
+++ houseprice/pipelines.py
@@ -3,14 +3,16 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline
 
+from houseprice.testredis import r
 
 class HousepricePipeline(object):
     def process_item(self, item, spider):
         print("HousepricePipeline-------->",item)
         return item
     def open_spider(self, spider):
+        print(r.get('gender'))
         print("open_spider------------------->",spider)
     def close_spider(self, spider):
         print("close_spider------------------->",spider)
