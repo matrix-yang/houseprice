41afe6e306390746e978e9f5052c3606c7acd0d2
Matrix-yang <739260804@qq.com>
2017-12-03 20:09:29 +0800

    加utf-8编码 # coding=UTF-8 # -*- coding:UTF-8 -*-

--- houseprice/spiders/first.py
+++ houseprice/spiders/first.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import scrapy;
 
 from houseprice.items import HousepriceItem
--- houseprice/testredis.py
+++ houseprice/testredis.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 import redis    # 导入redis模块，通过python操作redis 也可以直接在redis主机的服务端操作缓存数据库
 
 pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)   # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
